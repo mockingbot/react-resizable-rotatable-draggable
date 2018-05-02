6c0528898d4b97e8b3e976c8f75263c6cad9102d
Wei Wang <weiwang@wustl.edu>
2018-05-02 15:44:19 +0800

    remove style

--- src/index.example.js
+++ src/index.example.js
@@ -5,31 +5,6 @@ import styled from 'styled-components'
 
 const RootDiv = styled.div`
 text-align: center;
-
-.App-logo {
-  animation: App-logo-spin infinite 20s linear;
-  height: 80px;
-}
-
-.App-header {
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-}
-
-.App-title {
-  font-size: 1.5em;
-}
-
-.App-intro {
-  font-size: large;
-}
-
-@keyframes App-logo-spin {
-  from { transform: rotate(0deg); }
-  to { transform: rotate(360deg); }
-}
 `
 
 class AppExample extends Component {
