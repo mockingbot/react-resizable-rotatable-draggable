8b3a4203922d1b3ff650664a7239a028b28764e8
ciddarthjrj <ciddarthraaj@sportvot.com>
2023-02-23 09:04:21 +0530

    Adds is focused

--- src/Rect/index.js
+++ src/Rect/index.js
@@ -33,6 +33,13 @@ export default class Rect extends PureComponent {
     children: PropTypes.node
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFocused: false
+    }
+  }
+
   setElementRef = (ref) => {
     this.$element = ref
   }
@@ -157,6 +164,7 @@ export default class Rect extends PureComponent {
       parentRotateAngle,
       children
     } = this.props
+
     const style = {
       width: Math.abs(width),
       height: Math.abs(height),
@@ -164,51 +172,62 @@ export default class Rect extends PureComponent {
       left: centerX - Math.abs(width) / 2,
       top: centerY - Math.abs(height) / 2
     }
+
     const direction = zoomable
       .split(',')
       .map((d) => d.trim())
       .filter((d) => d) // TODO: may be speed up
 
+    const { isFocused } = this.state
+
     return (
-      <div
-        ref={this.setElementRef}
-        onMouseDown={this.startDrag}
-        className="rect single-resizer"
-        style={style}
-      >
-        {rotatable && (
-          <div className="rotate" onMouseDown={this.startRotate}>
-            <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M10.536 3.464A5 5 0 1 0 11 10l1.424 1.425a7 7 0 1 1-.475-9.374L13.659.34A.2.2 0 0 1 14 .483V5.5a.5.5 0 0 1-.5.5H8.483a.2.2 0 0 1-.142-.341l2.195-2.195z"
-                fill="#eb5648"
-                fillRule="nonzero"
-              />
-            </svg>
+      <>
+        {isFocused ? (
+          <div
+            ref={this.setElementRef}
+            onMouseDown={this.startDrag}
+            className="rect single-resizer"
+            style={style}
+          >
+            {rotatable && (
+              <div className="rotate" onMouseDown={this.startRotate}>
+                <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M10.536 3.464A5 5 0 1 0 11 10l1.424 1.425a7 7 0 1 1-.475-9.374L13.659.34A.2.2 0 0 1 14 .483V5.5a.5.5 0 0 1-.5.5H8.483a.2.2 0 0 1-.142-.341l2.195-2.195z"
+                    fill="#eb5648"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </div>
+            )}
+
+            {direction.map((d) => {
+              const cursor = `${getCursor(
+                rotateAngle + parentRotateAngle,
+                d
+              )}-resize`
+              return (
+                <div
+                  key={d}
+                  style={{ cursor }}
+                  className={`${zoomableMap[d]} resizable-handler`}
+                  onMouseDown={(e) => this.startResize(e, cursor)}
+                />
+              )
+            })}
+
+            {direction.map((d) => {
+              return <div key={d} className={`${zoomableMap[d]} square`} />
+            })}
+
+            <div className="childContainer">{children}</div>
+          </div>
+        ) : (
+          <div style={style} className="childContainer">
+            {children}
           </div>
         )}
-
-        {direction.map((d) => {
-          const cursor = `${getCursor(
-            rotateAngle + parentRotateAngle,
-            d
-          )}-resize`
-          return (
-            <div
-              key={d}
-              style={{ cursor }}
-              className={`${zoomableMap[d]} resizable-handler`}
-              onMouseDown={(e) => this.startResize(e, cursor)}
-            />
-          )
-        })}
-
-        {direction.map((d) => {
-          return <div key={d} className={`${zoomableMap[d]} square`} />
-        })}
-
-        <div className="childContainer">{children}</div>
-      </div>
+      </>
     )
   }
 }
